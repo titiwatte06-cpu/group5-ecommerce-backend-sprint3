e4d6d13a35232ea931f62df5300f3e4ac7051f04
toonajuig <thirawat.devtt@gmail.com>
2026-06-04 12:11:54 +0700

    feat: add admin stats and orders endpoints

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { router as productsRouter } from "./products.routes.js";
 import { router as categoriesRouter } from "./categories.routes.js";
 import { router as userRouter } from "./user.routes.js";
 import { router as orderRouter} from "./order.routes.js"
+import { router as adminRouter } from "./admin.routes.js";
 
 export const router = Router();
 
@@ -10,3 +11,4 @@ router.use("/products", productsRouter);
 router.use("/categories", categoriesRouter);
 router.use("/users", userRouter);
 router.use("/orders",orderRouter);
+router.use("/admin", adminRouter);
